845389f71866b82db41c3190b0719bde32a4526c
Megan Arellano <ssciolist@gmail.com>
2018-08-15 05:12:37 -0600

    Adds route, controller and view to support homepage. Fixes test typo

--- spec/features/user_can_check_daily_tubing_conditions_spec.rb
+++ spec/features/user_can_check_daily_tubing_conditions_spec.rb
@@ -5,9 +5,9 @@ describe 'As a user' do
     it 'tells me if today is a good day to go tubing' do
       visit '/'
 
-      expect(page).to have_context("Is today a good day to go tubing?")
-      expect(page).to have_context("No")
-      expect(page).to have_context("It's 59 degrees, partly cloudy and there's enough river flow for tubing")
+      expect(page).to have_content("Is today a good day to go tubing in Boulder Creek?")
+      expect(page).to have_content("No")
+      expect(page).to have_content("It's 59 degrees, partly cloudy and there's enough river flow for tubing")
     end
   end
 end
